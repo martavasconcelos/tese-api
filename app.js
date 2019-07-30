d1032e4974bf8023a6d767b9a284374df62a8629
martavasconcelos <martavasconcelos14@gmail.com>
2019-07-30 22:58:06 +0100

    Remove unused files. Refactor on app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var neo4j = require('neo4j-driver').v1;
+let express = require('express');
+let path = require('path');
+let logger = require('morgan');
+let bodyParser = require('body-parser');
+let neo4j = require('neo4j-driver').v1;
 
-var app = express();
+let app = express();
 
 //View engine
 app.set('views', path.join(__dirname, 'views'));
@@ -13,67 +13,112 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
-var driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', '1234'));
+var driver = neo4j.driver('bolt://10.227.107.156', neo4j.auth.basic('neo4j', 'tese2018'));
 var session = driver.session();
 
-app.get('/', function(req, res) {
-  session
-    .run('MATCH(n) RETURN n LIMIT 25')
-    .then(function(result) {
-      console.log(result)
-    })
-    .catch(function(err) {
-      console.log(err)
-    })
+app.get('/', function (req, res) {
+    session
+        .run('MATCH(n) RETURN n LIMIT 25')
+        .then(function (result) {
+        })
+        .catch(function (err) {
+            console.log(err)
+        })
 });
 
-app.post('/node/add', function(req, res) {
-  session.run('CREATE(n:OBJECT {value:{valueParam}, session:{sessionParam}, elementPos:{elementPosParam}, action: {actionParam}}) RETURN n', {
-      valueParam: req.body.value,
-      sessionParam: req.body.session,
-      elementPosParam: parseInt(req.body.elementPos),
-      actionParam: req.body.action
-  })
-    .then(function(result) {
-      console.log("sucess!")
+app.get('/lastId', function (req, res) {
+    session
+        .run('MATCH (n:OBJECT) RETURN n.pathId AS count ORDER BY count desc')
+        .then(function (result) {
+            res.json(result);
+        })
+        .catch(function (err) {
+            console.log(err)
+        })
+});
 
-      session.close();
+app.post('/pathId', function (req, res) {
+    session
+        .run('Match (n:OBJECT {path:{pathParam}, action:{actionParam}, url:{urlParam}}) RETURN n.pathId', {
+            pathParam: req.body.path,
+            actionParam: req.body.action,
+            urlParam: req.body.url,
+        })
+        .then(function (result) {
+            res.json(result);
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+});
+
+app.post('/node/add', function (req, res) {
+    session.run('MATCH (a:OBJECT) WHERE a.session = {sessionParam} AND a.elementPos = {elementPosBParam} return a', {
+        sessionParam: req.body.session,
+        elementPosBParam: parseInt(req.body.elementPos),
     })
-    .catch(function(err) {
-      console.log("error!")
-      console.log("error!", err)
+        .then(function (result) {
+            if (result.records.length === 0) {
+                session.run('CREATE(n:OBJECT {path:{pathParam}, pathId:{pathParamId}, session:{sessionParam}, elementPos:{elementPosParam}, action: {actionParam}, actionId: {actionParamId}, url: {urlParam}, value:{valueParam}}) RETURN n', {
+                    pathParam: req.body.path,
+                    pathParamId: req.body.pathId,
+                    sessionParam: req.body.session,
+                    elementPosParam: parseInt(req.body.elementPos),
+                    actionParam: req.body.action,
+                    actionParamId: req.body.actionId,
+                    urlParam: req.body.url,
+                    valueParam: req.body.value
+                })
+                    .then(function (result) {
+                        session.close();
+                    })
+            }
+        })
+        .catch(function (err) {
+            console.log("error!", err)
+        })
+});
+
+app.post('/relationship/add', function (req, res) {
+    session.run('MATCH (a:OBJECT) WHERE a.session = {sessionParam} AND a.elementPos = {elementPosBParam} return a', {
+        sessionParam: req.body.session,
+        elementPosBParam: parseInt(req.body.elementPos),
     })
+        .then(function (result) {
+            if (result.records.length === 0) {
+                session.run('MATCH (a:OBJECT) WHERE a.session = {sessionParam} AND a.elementPos = {elementPosAParam} CREATE(n:OBJECT {path: {pathParam}, pathId: {pathParamId}, session: {sessionParam}, elementPos: {elementPosBParam}, action: {actionParam}, actionId: {actionParamId}, url: {urlParam}, value: {valueParam}})-[:Follows]->(a) RETURN n', {
+                    pathParam: req.body.path,
+                    pathParamId: req.body.pathId,
+                    sessionParam: req.body.session,
+                    elementPosAParam: parseInt(req.body.elementPos) - 1,
+                    elementPosBParam: parseInt(req.body.elementPos),
+                    actionParam: req.body.action,
+                    actionParamId: req.body.actionId,
+                    urlParam: req.body.url,
+                    valueParam: req.body.value
+                })
+                    .then(function (result) {
+                        session.close();
+                    })
+            }
+        })
+        .catch(function (err) {
+            console.log("error!", err)
+        })
 
 });
 
-app.post('/relationship/add', function(req, res) {
-  session.run('MATCH (a:OBJECT) WHERE a.session = {sessionParam} AND a.elementPos = {elementPosAParam} CREATE(n:OBJECT {value: {valueParam}, session: {sessionParam}, elementPos: {elementPosBParam}, action: {actionParam}})-[:Follows]->(a) RETURN n', {
-      valueParam: req.body.value,
-      sessionParam: req.body.session,
-      elementPosAParam: parseInt(req.body.elementPos) - 1,
-      elementPosBParam: parseInt(req.body.elementPos),
-      actionParam: req.body.action
-    })
-    .then(function(result) {
-      console.log("sucess!")
 
-      session.close();
-    })
-    .catch(function(err) {
-      console.log("error!")
-      console.log("error!", err)
-    })
-
-});
-
-
-
-
-app.listen(3000);
-console.log('Server Started on Port 3000');
+app.listen(80, '10.227.107.156');
+console.log('Server Started on Port 8080');
 
 module.exports = app;
